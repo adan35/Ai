f5fe86bc529299f572123644b0ac37fb9e6bac07
adan35 <adanshahzad35@gmail.com>
2022-07-31 17:28:05 +0500

    BFS

--- BFS/BFS.py
+++ BFS/BFS.py
@@ -7,3 +7,20 @@ graph = {
     '8': []
 }
 
+def BFS(src, graph):
+    queue = []
+    visited = []
+    queue.append(src)
+    
+    # loop through queue while it's become empty
+    while len(queue) != 0:
+        node = queue.pop(0)
+        # print the node
+        print(node, end=' ')
+        # loop through each children of the node
+        for child in graph[node]:
+            if child not in visited:
+                visited.append(child)
+                queue.append(child)
+
+BFS('5', graph)
